315174723cf123d96842379ae02157ca6fdb54bf
RedmiLine <145707407+RedmiLine@users.noreply.github.com>
2023-09-22 20:46:49 +0800

    Update config.fish
    https://github.com/JanDeDobbeleer/oh-my-posh/edit/main/.devcontainer/config.fish

--- .devcontainer/config.fish
+++ .devcontainer/config.fish
@@ -2,3 +2,4 @@
 oh-my-posh init fish --config $POSH_THEME | source
 
 # NOTE: You can override the above env vars from the devcontainer.json "args" under the "build" key.
+# SEND: Point under from pastebin "build" key $POSH SkyEye
